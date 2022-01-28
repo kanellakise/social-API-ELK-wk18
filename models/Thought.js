834e6056c145b6dbd239c447e6c3bc281a2a2859
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-28 13:43:59 -0600

    added Thought schema model

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
