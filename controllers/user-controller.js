276306abf2d8d742cd4c98637fd70dec68f838ed
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-29 16:47:45 -0600

    corrected user route parameters

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,7 +14,8 @@ const userController = {
 
   //get one User by id
   getUserById({ params }, res) {
-    User.findOne({ _id: params.id })
+    console.log(params);
+    User.findOne({ _id: params.userId })
       .populate({
         path: 'thoughts',
         select: '-__v'
@@ -43,7 +44,7 @@ const userController = {
 
   // Update User by id
   updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    User.findOneAndUpdate({ _id: params.userId }, body, { new: true, runValidators: true })
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
@@ -56,7 +57,7 @@ const userController = {
 
   // Delete User
   deleteUser({ params }, res) {
-    User.findOneAndDelete({ _id: params.id })
+    User.findOneAndDelete({ _id: params.userId })
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user found with this id!' });
